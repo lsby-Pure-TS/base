bcfca2239073c8976d5b6e4b1a847d46cf1858cd
hbybyyang <2451759073@qq.com>
2022-09-29 18:19:01 +0800

    尝试引入高阶类型算子

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export * from "./Function";
 export * from "./类型代表";
 export * from "./函子";
 export * from "./单子";
+export * from "./K1";
+export * from "./K2";
+export * from "./K3";
+export * from "./K4";
+export * from "./K5";
